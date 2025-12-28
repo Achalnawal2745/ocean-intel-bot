6e1e2d8c88abe580f59ec79ac2d89eb8c0377eb2
Achalnawal2745 <achalnawal2005@gmail.com>
2025-12-28 15:52:08 +0530

    clean files

--- test_http_download.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Try with a float we know exists (1902669)
-response = requests.post(
-    "http://127.0.0.1:8000/admin/download-float",
-    json={"float_id": "1902669"}
-)
-
-print("Response:", response.json())
